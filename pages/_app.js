dede65e3ad7b178991f68aa468cbd836ed6a339f
hovelopin <kimhojin3714@naver.com>
2022-10-11 23:01:21 +0900

    02-03. Next.js 기본3 ( Layouts-Pages-Image )

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+// 모든 페이지를 품을 수 있는 구조
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return (
+  //   <Layout>
+  //     <Component {...pageProps} />
+  //   </Layout>
+  // );
+
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+
+  return getLayout(<Component {...pageProps} />);
 }
 
-export default MyApp
+export default MyApp;
